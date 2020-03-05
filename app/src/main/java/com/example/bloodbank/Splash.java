b78fc42a5839a78001b0bde67dfaeb5ebc2e8535
Ramadhuvishan <42070850+Ramadhuvishan@users.noreply.github.com>
2020-03-05 18:44:07 +0530

    Update Splash.java

--- app/src/main/java/com/example/bloodbank/Splash.java
+++ app/src/main/java/com/example/bloodbank/Splash.java
@@ -1,14 +1,25 @@
 package com.example.bloodbank;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
-public class Splash extends AppCompatActivity {
+public class Splash extends Activity {
+    private static int spTime = 3000;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent i = new Intent(Splash.this,MainActivity.class);
+                startActivity(i);
+                finish();
+            }
+        },spTime);
     }
+
 }
