23442aa475573769b7ca14e42e1577d356f9a6d4
Ramadhuvishan <42070850+Ramadhuvishan@users.noreply.github.com>
2020-03-05 18:42:48 +0530

    Update Quantity.java

--- app/src/main/java/com/example/bloodbank/Quantity.java
+++ app/src/main/java/com/example/bloodbank/Quantity.java
@@ -3,6 +3,7 @@ package com.example.bloodbank;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.WindowManager;
 
 public class Quantity extends AppCompatActivity {
 
@@ -10,5 +11,8 @@ public class Quantity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quantity);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        getSupportActionBar().hide();
     }
 }
